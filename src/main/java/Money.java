dda43e8dbeeab9b9a8ba0fc05d4e032f2a256012
bpawluczuk <b.pawluczuk@companion.pl>
2020-05-08 16:18:01 +0200

    Money

---
+++ src/main/java/Money.java
@@ -0,0 +1,49 @@
+public class Money implements Expression {
+
+    protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    public Expression times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
+    public String currency() {
+        return currency;
+    }
+
+    public Expression plus(Expression addend) {
+        return new Sum(this, addend);
+    }
+
+    public Money reduce(Bank bank, String to) {
+        int rate = bank.rate(currency, to);
+        return new Money(amount / rate, to);
+    }
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount
+                && this.currency().equals(money.currency);
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+}
